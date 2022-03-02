fcfb0dc43b4d7b6481f3336bbec6ac67f8ca9157
opilane <opilane@HP-CZC6297BB4>
2022-03-02 10:55:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nelinurk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Nelinurk figura = new Nelinurk(4,3);
+            figura.Massiiv();
+            figura.Vivod();
+            Console.ReadKey();
+        }
+    }
+}
